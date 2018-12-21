ece7bdd5991aadbfcb28a8b510ed53cdbc391f99
gunes <gunes.buyukgonenc@ozu.edu.tr>
2018-12-21 12:19:13 +0300

    Simple Factory for Dialog Fragment added

--- app/src/main/java/sudoku/cobyapps/com/sudoku/BuildSudoku.java
+++ app/src/main/java/sudoku/cobyapps/com/sudoku/BuildSudoku.java
@@ -4,49 +4,77 @@ import java.util.Random;
 
 public class BuildSudoku {
 
-    private int numberOfUnknowns;
-    private int lowerBoundary;
-    private int upperBoundary;
+    private int numberOfUnknownsLowerBound;
+    private int numberOfUnknownsUpperBound;
+    private int difficultyLowerBoundary;
+    private int difficultyUpperBoundary;
 
     private BuildSudoku(Builder builder) {
-        this.numberOfUnknowns = builder.numberOfUnknowns;
-        this.lowerBoundary = builder.lowerBoundary;
-        this.upperBoundary = builder.upperBoundary;
+        this.numberOfUnknownsLowerBound = builder.numberOfUnknownsLowerBound;
+        this.numberOfUnknownsUpperBound = builder.numberOfUnknownsUpperBound;
+        this.difficultyLowerBoundary = builder.difficultyLowerBoundary;
+        this.difficultyUpperBoundary = builder.difficultyUpperBoundary;
     }
 
     public static class Builder {
 
-        private int numberOfUnknowns ;
-        private int lowerBoundary;
-        private int upperBoundary;
+        private int numberOfUnknownsLowerBound;
+        private int numberOfUnknownsUpperBound;
+        private int difficultyLowerBoundary;
+        private int difficultyUpperBoundary;
 
         private Random random = new Random();
 
+
+        // TODO: 20.12.2018 edit numbers
         public Builder() {
-            numberOfUnknowns = 20;
-            lowerBoundary = 5;
-            upperBoundary = 120;
+            numberOfUnknownsLowerBound = 20;
+            numberOfUnknownsUpperBound = 40;
+            difficultyLowerBoundary = 5;
+            difficultyUpperBoundary = 120;
         }
 
+        // TODO: 20.12.2018 edit numbers
         public Builder allRandom(){
-            this.numberOfUnknowns = random.nextInt(61) + 1;
-            this.lowerBoundary = random.nextInt(16) + 5;
-            this.upperBoundary = random.nextInt(81) + 40;
+            this.numberOfUnknownsLowerBound = random.nextInt(10) + 20;
+            this.numberOfUnknownsUpperBound = random.nextInt(21) + 40;
+            this.difficultyLowerBoundary = random.nextInt(16) + 5;
+            this.difficultyUpperBoundary = random.nextInt(81) + 40;
             return this;
         }
 
-        public Builder withNumberOfUnknowns(int numberOfUnknowns) {
-            this.numberOfUnknowns = numberOfUnknowns;
+        public Builder withNumberOfUnknownsLowerBound(int numberOfUnknownsLowerBound) throws Exception {
+            if (numberOfUnknownsLowerBound >= 0 && numberOfUnknownsLowerBound < 65) {
+                this.numberOfUnknownsLowerBound = numberOfUnknownsLowerBound;
+                this.numberOfUnknownsLowerBound = numberOfUnknownsLowerBound;
+            } else
+                throw new Exception("Boundaries must be between 0 and 64");
             return this;
         }
 
-        public Builder withLowerBound(int lowerBoundary) {
-            this.lowerBoundary = lowerBoundary;
+        public Builder withNumberOfUnknownsUpperBound(int numberOfUnknownsUpperBound) throws Exception {
+            if (numberOfUnknownsUpperBound >= 0 && numberOfUnknownsUpperBound < 65) {
+                this.numberOfUnknownsUpperBound = numberOfUnknownsUpperBound;
+                this.numberOfUnknownsUpperBound = numberOfUnknownsUpperBound;
+            } else
+                throw new Exception("Boundaries must be between 0 and 64");
             return this;
         }
 
-        public Builder withUpperBound(int upperBoundary) {
-            this.upperBoundary = upperBoundary;
+        public Builder withDifficultyLowerBound(int difficultyLowerBound) throws Exception {
+            if (difficultyLowerBound >= 0) {
+                this.difficultyLowerBoundary = difficultyLowerBound;
+            } else
+                throw new Exception("Difficulty must be positive");
+            return this;
+        }
+
+        public Builder withDifficultyUpperBound(int difficultyUpperBound) throws Exception {
+            if (difficultyUpperBoundary >= 0) {
+                this.difficultyUpperBoundary = difficultyUpperBound;
+            } else {
+                throw new Exception("Difficulty must be positive");
+            }
             return this;
         }
 
--- app/src/main/java/sudoku/cobyapps/com/sudoku/Fragments/ConfirmationDialogFragment.java
+++ app/src/main/java/sudoku/cobyapps/com/sudoku/Fragments/ConfirmationDialogFragment.java
@@ -1,11 +1,13 @@
 package sudoku.cobyapps.com.sudoku.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class ConfirmationDialogFragment extends DialogFragmentWithTwoButtons {
     private static final String TITLE = "Confirm delete?";
     private static final String LEFT_BUTTON_TEXT = "Delete";
     private static final String RIGHT_BUTTON_TEXT = "Cancel";
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         title.setText(TITLE);
