a5002a53e41134a0190716fd7b8b971fd08a1058
gunes <gunes.buyukgonenc@ozu.edu.tr>
2018-12-26 17:42:08 +0300

    nani

---
+++ app/src/main/java/sudoku/cobyapps/com/sudoku/Utilities/DatePicker.java
@@ -0,0 +1,26 @@
+package sudoku.cobyapps.com.sudoku.Utilities;
+
+import android.content.Context;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import sudoku.cobyapps.com.sudoku.MainActivity;
+
+public class DatePicker {
+
+    public static String getCurrentDate(Context context) {
+        Locale current = context.getResources().getConfiguration().locale;
+        Date date = Calendar.getInstance().getTime();
+        if (current.getCountry().equals(Locale.US.toString())) {
+            DateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy hh:mm:ss");
+            return dateFormat.format(date);
+        } else {
+            DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
+            return dateFormat.format(date);
+        }
+    }
+}
